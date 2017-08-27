5408c53977accd2fbfa1fbd37489040723efb12b
Philip Savarirayan <philip.sav@gmail.com>
2017-08-27 21:29:41 +0530

    rocking

--- js/rocket.js
+++ js/rocket.js
@@ -6,11 +6,24 @@ var changeState = function (state) {
     countdownNumber = 10;
     document.getElementById('countdown').innerHTML = countdownNumber;
 
+    //countdown
     if (state == 2) {
         timer = setInterval(function () {
             countdownNumber--;
-
             document.getElementById('countdown').innerHTML = countdownNumber;
+
+            if (countdownNumber > 4 && countdownNumber <= 7) {
+                document.getElementById('nervous').className = 'nervous show';
+            } else {
+                document.getElementById('nervous').className = 'nervous';
+            }
+
+            if (countdownNumber > 1 && countdownNumber <= 4) {
+                document.getElementById('cant-wait').className = 'cant-wait show';
+            } else {
+                document.getElementById('cant-wait').className = 'cant-wait ';
+            }
+
             if (countdownNumber <= 0) {
                 changeState(3);
             };
@@ -20,7 +33,7 @@ var changeState = function (state) {
             var randomNumber = Math.round(Math.random() * 10);
             console.log('randomNumber:' +
                 randomNumber)
-            if (randomNumber > 5) {
+            if (randomNumber > 2) {
                 changeState(4);
             } else {
                 changeState(5);
